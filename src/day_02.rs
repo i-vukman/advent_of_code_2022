11a5f9e7a443c5687b75fd777bd928ed1fc097eb
Ivan Vukman <ivan.vukman@gmail.com>
2022-12-04 02:25:08 +0100

    Solve day 2 part 2

--- src/day_02.rs
+++ src/day_02.rs
@@ -8,10 +8,6 @@ pub mod day_02 {
         Scissors = 3,
     }
 
-    trait EncryptedMove {
-        fn decrypt(&self) -> Move;
-    }
-
     enum EncryptedOpponentMove {
         A,
         B,
@@ -31,7 +27,7 @@ pub mod day_02 {
         }
     }
 
-    impl EncryptedMove for EncryptedOpponentMove {
+    impl EncryptedOpponentMove {
         fn decrypt(&self) -> Move {
             match self {
               EncryptedOpponentMove::A => Move::Rock,
@@ -60,23 +56,62 @@ pub mod day_02 {
         }
     }
 
-    impl EncryptedMove for EncryptedPlayerMove {
-        fn decrypt(&self) -> Move {
-            match self {
+    pub fn get_total_score_1(encrypted_input: String) -> u32 {
+        let decrypt_player_move = |_opponent_move: &EncryptedOpponentMove, player_move: &EncryptedPlayerMove| {
+            match player_move {
                 EncryptedPlayerMove::X => Move::Rock,
                 EncryptedPlayerMove::Y => Move::Paper,
                 EncryptedPlayerMove::Z => Move::Scissors,
             }
+        };
+
+        get_total_score(encrypted_input, decrypt_player_move)
+    }
+
+    pub fn get_total_score_2(encrypted_input: String) -> u32 {
+        let decrypt_player_move = |opponent_move: &EncryptedOpponentMove, player_move: &EncryptedPlayerMove| {
+            match player_move {
+                EncryptedPlayerMove::X => get_losing_counter_move(&opponent_move.decrypt()),
+                EncryptedPlayerMove::Y => get_draw_counter_move(&opponent_move.decrypt()),
+                EncryptedPlayerMove::Z => get_winning_counter_move(&opponent_move.decrypt()),
+            }
+        };
+
+        get_total_score(encrypted_input, decrypt_player_move)
+    }
+
+    fn get_winning_counter_move(m: &Move) -> Move {
+        match m {
+            Move::Paper => Move::Scissors,
+            Move::Rock => Move::Paper,
+            Move::Scissors => Move::Rock,
         }
     }
 
-    pub fn get_total_score(encrypted_input: String) -> u32 {
+    fn get_draw_counter_move(m: &Move) -> Move {
+        match m {
+          Move::Paper => Move::Paper,
+          Move::Rock => Move::Rock,
+          Move::Scissors => Move::Scissors,
+        }
+    }
+
+    fn get_losing_counter_move(m: &Move) -> Move {
+        match m {
+            Move::Paper => Move::Rock,
+            Move::Rock => Move::Scissors,
+            Move::Scissors => Move::Paper
+        }
+    }
+
+    fn get_total_score<F>(encrypted_input: String, decrypt_player_move: F) -> u32  where 
+        F: Fn(&EncryptedOpponentMove, &EncryptedPlayerMove) -> Move {
         encrypted_input
             .lines()
             .map(|line| line.split(' ').collect::<Vec<_>>())
             .map(|move_pair| (EncryptedOpponentMove::from_str(move_pair[0]).unwrap(), 
                             EncryptedPlayerMove::from_str(move_pair[1]).unwrap()))
-            .map(|move_pair| (move_pair.0.decrypt(), move_pair.1.decrypt()))
+            .map(|move_pair| (move_pair.0.decrypt(), decrypt_player_move(&move_pair.0, &move_pair.1)))
             .map(|move_pair| {
                 let opponent_move = move_pair.0;
                 let player_move = move_pair.1;
@@ -85,8 +120,8 @@ pub mod day_02 {
                     Some(winning_move) => return if winning_move == player_move { 6 + player_move as u32 } else { 0 + player_move as u32 },
                     None => return 3 + player_move as u32
                 }
-            })
-            .sum()
+        })
+        .sum()
     }
 
     fn get_winning_move(first: &Move, second: &Move) -> Option<Move> {
@@ -114,12 +149,19 @@ pub mod day_02 {
 mod tests {
     use std::fs;
 
-    use super::day_02::get_total_score;
+    use super::day_02::{get_total_score_1, get_total_score_2};
 
     #[test]
-    fn test_from_input() {
+    fn test_part_1_from_input() {
         let input = fs::read_to_string("input/day_02.txt").unwrap();
 
-        assert_eq!(get_total_score(input), 10595);
+        assert_eq!(get_total_score_1(input), 10595);
+    }
+
+    #[test]
+    fn test_part_2_from_input() {
+        let input = fs::read_to_string("input/day_02.txt").unwrap();
+
+        assert_eq!(get_total_score_2(input), 9541);
     }
 }
