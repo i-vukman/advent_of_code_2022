c0d74cd9fe4c80e7fc5ab73343a1f0318c2fccbe
Ivan Vukman <ivan.vukman@gmail.com>
2022-12-14 00:47:59 +0100

    Clean up day 7

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use day_07::{self, terminal_output::line::Line, file_system::file_system_node::{FileSystemNode}};
+
+fn main() -> color_eyre::Result<()> {
+    let input = include_str!("../input/day_07_sample.txt");
+    let lines = Line::parse_lines(input);
+    let root = FileSystemNode::build_from_lines(lines)?;
+
+    println!("{:#?}", root);
+
+    Ok(())
 }
--- day_07/tests/integration_test.rs
+++ day_07/tests/integration_test.rs
@@ -1,13 +1,13 @@
 #[test]
 fn test_total_small_files() {
     let input = include_str!("../input/day_07.txt");
-    let result = day_07_take_02::calculate_total_size_of_dirs_smaller_than(input, 100000);
+    let result = day_07::calculate_total_size_of_dirs_smaller_than(input, 100000);
     assert_eq!(result, 1648397);
 }
 
 #[test]
 fn test_smallest_file_to_delete() {
     let input = include_str!("../input/day_07.txt");
-    let result = day_07_take_02::get_min_file_size_to_free_up_storage(input, 70000000, 30000000).unwrap();
+    let result = day_07::get_min_file_size_to_free_up_storage(input, 70000000, 30000000).unwrap();
     assert_eq!(result, 1815525);
 }
