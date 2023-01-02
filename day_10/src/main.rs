0a2cb6aa27e934a1b5d6827f087bc7bcc87b2734
Ivan Vukman <ivan.vukman@gmail.com>
2023-01-02 23:28:59 +0100

    day 10 part 2

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -1,7 +1,7 @@
 use day_10::{instruction::Instruction, build_crt_output};
 
 fn main() {
-    let input = include_str!("../input/sample.txt");
+    let input = include_str!("../input/day_10.txt");
     let instructions = Instruction::parse_instructions(input); 
     let output = build_crt_output(&instructions);
     for line in output {
