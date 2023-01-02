010f1a4fc790b4e35a6abe614e21ec25572b2f63
Ivan Vukman <ivan.vukman@gmail.com>
2023-01-02 22:09:06 +0100

    day 10 part 1

--- day_10/tests/integration_test.rs
+++ day_10/tests/integration_test.rs
@@ -8,3 +8,12 @@ fn test_signal_strenght_from_sample() {
 
     assert_eq!(sum, 13140);
 }
+
+#[test]
+fn test_signal_strength_from_input() {
+    let input = include_str!("../input/day_10.txt");
+    let instructions = Instruction::parse_instructions(input);
+    let sum = day_10::sum_signal_strenghts(&instructions);
+
+    assert_eq!(sum, 12980);
+}
